adcf6f993dd2e61013959a6fafb46e733a308504
Godzilla-DST <86336213+Godzilla-DST@users.noreply.github.com>
2021-07-01 15:11:22 +0200

    Delete name.py

--- name.py
+++
@@ -1,9 +0,0 @@
-def name() :
-    import os
-    import godzilla
-    os.system("clear")
-    user = input("user name : ")
-    if user == "DST" : 
-        godzilla.godzilla()
-    else :
-        os.system("exit")
